27c22376cce170939d28507ba64603a612c768a5
vfurmane <vfurmane@student.42.fr>
2021-01-06 14:11:35 +0100

    Add tests for split

---
+++ test/tests/ft_split_test.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split_test.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfurmane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/30 15:14:46 by vfurmane          #+#    #+#             */
+/*   Updated: 2021/01/06 13:52:36 by vfurmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tests.h"
+
+char	*ref[5] = { "Hello", "World", "and", "42", NULL };
+char	*ref2[2] = { "Hello World and 42", NULL };
+char	**ret;
+
+int		ft_arrlen(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+		i++;
+	return (i);
+}
+
+void	ft_split_basic(void)
+{
+	char	*str = "Hello World and 42";
+	char	sep = ' ';
+
+	ret = ft_split(str, sep);
+	TEST_ASSERT_EQUAL_STRING_ARRAY(ref, ret, ft_arrlen(ret) + 1);
+}
+
+void	ft_split_long_sep(void)
+{
+	char	*str = "        Hello    World     and       42          ";
+	char	sep = ' ';
+
+	ret = ft_split(str, sep);
+	TEST_ASSERT_EQUAL_STRING_ARRAY(ref, ret, ft_arrlen(ret) + 1);
+}
+
+void	ft_split_no_sep(void)
+{
+	char	*str = "Hello World and 42";
+	char	sep = 'y';
+
+	ret = ft_split(str, sep);
+	TEST_ASSERT_EQUAL_STRING_ARRAY(ref2, ret, ft_arrlen(ret) + 1);
+}
+
+void	ft_split_zero_sep(void)
+{
+	char	*str = "Hello World and 42";
+	char	sep = '\0';
+
+	ret = ft_split(str, sep);
+	TEST_ASSERT_EQUAL_STRING_ARRAY(ref2, ret, ft_arrlen(ret) + 1);
+}
+
+/* -------------------------------------------------------------------------- */
+
+void	setUp(void)
+{
+}
+
+void	tearDown(void)
+{
+	/*
+	int	i;
+
+	i = 0;
+	while (ret[i])
+	{
+		free(ret[i]);
+	}
+	free(ret);
+	*/
+}
+
+int		main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(ft_split_basic);
+	RUN_TEST(ft_split_long_sep);
+	RUN_TEST(ft_split_no_sep);
+	RUN_TEST(ft_split_zero_sep);
+	return (UNITY_END());
+}
