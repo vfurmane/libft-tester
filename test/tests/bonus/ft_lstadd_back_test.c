f6872842e9cdd8ac1902c74e881e28fd85975c90
vfurmane <vfurmane@student.42.fr>
2021-01-09 11:45:24 +0100

    Add test for null ptr

--- test/tests/bonus/ft_lstadd_back_test.c
+++ test/tests/bonus/ft_lstadd_back_test.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/30 15:14:46 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/01/08 10:34:32 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/01/09 11:43:13 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,30 @@
 
 t_list	*ret;
 t_list	*new_lst;
+t_list	*new_lst2;
 t_list	*initial_lst;
 
 void	ft_lstadd_back_basic(void)
 {
 	char	*str = "Hello World";
 
+	ret = ft_lstnew("Ref content");
+	initial_lst = ret;
 	new_lst = ft_lstnew(str);
 	ft_lstadd_back(&ret, new_lst);
+	TEST_ASSERT_EQUAL_STRING(str, ft_lstlast(ret)->content);
+	TEST_ASSERT_EQUAL_PTR(initial_lst->next, ft_lstlast(ret));
+}
+
+void	ft_lstadd_back_null_ptr(void)
+{
+	char	*str = "Hello World";
+
+	new_lst = ft_lstnew(str);
+	new_lst2 = ft_lstnew(str);
+	ret = NULL;
+	ft_lstadd_back(&ret, new_lst);
+	ft_lstadd_back(&ret, new_lst2);
 	TEST_ASSERT_EQUAL_STRING(str, ft_lstlast(ret)->content);
 	TEST_ASSERT_EQUAL_PTR(initial_lst->next, ft_lstlast(ret));
 }
@@ -30,6 +46,8 @@ void	ft_lstadd_back_non_ptr_content(void)
 {
 	int	nbr = 42;
 
+	ret = ft_lstnew("Ref content");
+	initial_lst = ret;
 	new_lst = ft_lstnew(&nbr);
 	ft_lstadd_back(&ret, new_lst);
 	TEST_ASSERT_EQUAL_INT(nbr, *((int*)ft_lstlast(ret)->content));
@@ -40,8 +58,6 @@ void	ft_lstadd_back_non_ptr_content(void)
 
 void	setUp(void)
 {
-	ret = ft_lstnew("Ref content");
-	initial_lst = ret;
 }
 
 void	tearDown(void)
@@ -54,6 +70,7 @@ int		main(void)
 {
 	UNITY_BEGIN();
 	RUN_TEST(ft_lstadd_back_basic);
+	RUN_TEST(ft_lstadd_back_null_ptr);
 	RUN_TEST(ft_lstadd_back_non_ptr_content);
 	return (UNITY_END());
 }
