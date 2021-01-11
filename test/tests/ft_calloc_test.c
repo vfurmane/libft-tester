b0fbcc63d532e520007b5a5a590dad234cd86c3e
vfurmane <vfurmane@student.42.fr>
2021-01-11 13:17:17 +0100

    Fix my tests

--- test/tests/ft_calloc_test.c
+++ test/tests/ft_calloc_test.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/30 15:14:46 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/01/06 11:24:10 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/01/11 13:14:36 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,17 @@ void	ft_calloc_zero_len(void)
 	int		len = 0;
 	int		size = sizeof(int);
 
-	ref = calloc(len, size);
 	ret = ft_calloc(len, size);
-	TEST_ASSERT_EQUAL_MEMORY(ref, ret, len * size);
+	TEST_ASSERT_NOT_NULL(ret);
+}
+
+void	ft_calloc_zero_size(void)
+{
+	int		len = 5;
+	int		size = 0;
+
+	ret = ft_calloc(len, size);
+	TEST_ASSERT_NOT_NULL(ret);
 }
 
 void	ft_calloc_negative_len(void)
@@ -59,5 +67,8 @@ int		main(void)
 {
 	UNITY_BEGIN();
 	RUN_TEST(ft_calloc_basic);
+	RUN_TEST(ft_calloc_zero_len);
+	RUN_TEST(ft_calloc_zero_size);
+	RUN_TEST(ft_calloc_negative_len);
 	return (UNITY_END());
 }
